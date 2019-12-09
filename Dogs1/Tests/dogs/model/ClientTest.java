c22022c9fd872df64d9061eca36d098952a46de4
Niko <Niko@DESKTOP-EAI91NO>
2019-12-08 19:52:09 -0500

    Avec Client tests

--- Dogs1/Tests/dogs/model/ClientTest.java
+++ Dogs1/Tests/dogs/model/ClientTest.java
@@ -20,4 +20,75 @@ public class ClientTest {
 		
 		Assert.assertEquals(EXPECTED_VALUE, ACTUAL_VALUE);
 	}
+	
+	@Test
+	public void CreateClient_getFirstName_returnGoodFirstName() {
+		
+		Client client = new Client(firstName, lastName, telephoneNumber);
+		final String EXPECTED_VALUE = firstName;
+		final String ACTUAL_VALUE = client.getFirstName();
+		
+		Assert.assertEquals(EXPECTED_VALUE, ACTUAL_VALUE);
+	}
+	
+	@Test
+	public void CreateClient_getLastName_returnGoodLastName() {
+		
+		Client client = new Client(firstName, lastName, telephoneNumber);
+		final String EXPECTED_VALUE = lastName;
+		final String ACTUAL_VALUE = client.getLastName();
+		
+		Assert.assertEquals(EXPECTED_VALUE, ACTUAL_VALUE);
+	}
+	
+	@Test
+	public void CreateClient_getPhoneNumber_returnGoodPhoneNumber() {
+		
+		Client client = new Client(firstName, lastName, telephoneNumber);
+		final String EXPECTED_VALUE = telephoneNumber;
+		final String ACTUAL_VALUE = client.getPhoneNumber();
+		
+		Assert.assertEquals(EXPECTED_VALUE, ACTUAL_VALUE);
+	}
+	
+	@Test
+	public void CreateClient_setFirstName_returnChangedPhoneNumber() {
+		
+		String secondName = "Niko";
+		Client client = new Client(firstName, lastName, telephoneNumber);
+		
+		client.setFirstName(secondName);
+		final String EXPECTED_VALUE = secondName;
+		final String ACTUAL_VALUE = client.getFirstName();
+		
+		Assert.assertEquals(EXPECTED_VALUE, ACTUAL_VALUE);
+	}
+	
+	@Test
+	public void CreateClient_setLastName_returnChangedPhoneNumber() {
+		
+		String secondName = "Gallant";
+		Client client = new Client(firstName, lastName, telephoneNumber);
+		
+		client.setLastName(secondName);
+		final String EXPECTED_VALUE = secondName;
+		final String ACTUAL_VALUE = client.getLastName();
+		
+		Assert.assertEquals(EXPECTED_VALUE, ACTUAL_VALUE);
+	}
+	
+	@Test
+	public void CreateClient_setPhoneNumber_returnChangedPhoneNumber() {
+		
+		String secondPhoneNumber = "(418) 123-4567";
+		Client client = new Client(firstName, lastName, telephoneNumber);
+		
+		client.setPhoneNumber(secondPhoneNumber);
+		final String EXPECTED_VALUE = secondPhoneNumber;
+		final String ACTUAL_VALUE = client.getPhoneNumber();
+		
+		Assert.assertEquals(EXPECTED_VALUE, ACTUAL_VALUE);
+	}
+	
+	
 }
